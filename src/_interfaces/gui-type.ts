d8b2f6ac84ac417d05281651e5bc2d1f29618781
Sreenivasa Reddy Inukollu <vasu@inukollu.com>
2021-03-30 13:06:10 +0530

    gui type parameter

---
+++ src/_interfaces/gui-type.ts
@@ -0,0 +1,10 @@
+export enum GuiType {
+    Unknown = 0,
+    Agent = 1,
+    TeamLead = 2,
+    QualityAnalyst = 3,
+    DataAnalyst = 4,
+    OperationsLead = 5,
+    Manager = 6,
+    ProcessOwner = 7,
+}
